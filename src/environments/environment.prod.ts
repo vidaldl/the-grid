5d9eebe73ba39d2b957cb3a2bc3d6cdcba4836ab
Vidal <vidaldl@byui.edu>
2022-08-31 13:45:52 -0600

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-angular0-grid',
+    appId: '1:256456001539:web:b86dbba964c24ca59a2636',
+    storageBucket: 'fir-angular0-grid.appspot.com',
+    apiKey: 'AIzaSyBckUiRTOgmSpkotv9Q3PkUtf2Yi7_VEq0',
+    authDomain: 'fir-angular0-grid.firebaseapp.com',
+    messagingSenderId: '256456001539',
+    measurementId: 'G-Q2X7JFZ9JT',
+  },
   production: true
 };
